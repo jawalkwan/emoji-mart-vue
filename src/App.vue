3e1cb4d379e11550f380dd01c877f74a144ff68e
delayhsiao <fuck@fackyou.org>
2024-01-29 21:57:32 +0800

    create component to implement emoji-mart

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import data from '@emoji-mart/data'
+import EmojiPicker from '@/packages/emoji-mart-vue/index.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="emoji-picker-container">
+    <EmojiPicker
+      :data="data"
+      locale="zh"
+      searchPosition="sticky"
+      skinTonePosition="search"
+      previewPosition="none"
+    />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/packages/emoji-mart-vue/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { Picker } from 'emoji-mart'
+import { nextTick, onMounted, ref, useAttrs } from 'vue'
+
+const attrs = useAttrs()
+const emojiPicker = ref()
+const instance = ref()
+
+onMounted(() => {
+  nextTick(() => {
+    let props = Object.assign({ parent: emojiPicker.value }, attrs)
+    if (instance.value == undefined) {
+      instance.value = new Picker({ ...props })
+    }
+  })
+})
+</script>
+
+<template>
+  <div class="emoji-picker" ref="emojiPicker"></div>
+</template>
+
+<style scoped></style>
